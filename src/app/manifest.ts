497f49bd5a10201ac8c4fd0e5a5baa4ff56c57e4
Nour Malaeb <nourmalaeb@gmail.com>
2025-01-02 15:18:35 -0500

    Add manifest and headers for PWA

---
+++ src/app/manifest.ts
@@ -0,0 +1,25 @@
+import type { MetadataRoute } from 'next';
+
+export default function manifest(): MetadataRoute.Manifest {
+  return {
+    name: 'daily protein club',
+    short_name: 'dp club',
+    description: 'track your daily protein intake, simply',
+    start_url: '/',
+    display: 'standalone',
+    background_color: '#212121',
+    theme_color: '#ededed',
+    icons: [
+      {
+        src: '/icon-192x192.png',
+        sizes: '192x192',
+        type: 'image/png',
+      },
+      {
+        src: '/icon-512x512.png',
+        sizes: '512x512',
+        type: 'image/png',
+      },
+    ],
+  };
+}
