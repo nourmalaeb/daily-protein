35130372f1b8004f9e839c9e8ca81deff1d70d25
Nour Malaeb <nourmalaeb@gmail.com>
2025-01-06 23:58:04 -0500

    Update signup flow

---
+++ src/app/(auth)/signup/confirm-email/page.tsx
@@ -0,0 +1,18 @@
+'use client';
+
+import Link from 'next/link';
+
+const SignupPage = () => {
+  // console.log(actionData);
+  return (
+    <div className="flex flex-col gap-10 p-4">
+      <h1 className="font-bold text-2xl tracking-tight">Check your inbox</h1>
+      <p>
+        Please check your email for a confirmation link. If you don’t see it,
+        check your spam folder.
+      </p>
+    </div>
+  );
+};
+
+export default SignupPage;
