da9ee0cc52ef7ba01d627f17b9bfa2d421d584ce
Nour Malaeb <nourmalaeb@gmail.com>
2025-08-19 16:57:29 -0400

    Move widgettester to a new branch

--- src/app/widgettester/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { MainWrapper } from '@/components/mainWrapper';
-
-export default function Layout({ children }: { children: React.ReactNode }) {
-  return (
-    <>
-      <MainWrapper className="overflow-auto">{children}</MainWrapper>
-    </>
-  );
-}
